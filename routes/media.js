6cb169326027875053c243662eaef375f6ce0b11
Balbaal <iqbal.mi20@gmail.com>
2020-09-03 21:50:00 +0800

    add: post -> handle upload image

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const isBase64 = require("is-base64");
+const base64Img = require("base64-img");
 
-/* GET media listing. */
-router.get('/', function(req, res, next) {
-  res.send('media service');
+// Models
+const { Media } = require("../models");
+
+router.post("/", async (req, res) => {
+  const { image } = req.body;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    res.status(400).json({
+      status: "error",
+      message: "image must be base64 data",
+    });
+  }
+
+  // Convert base 64 data to image file
+  base64Img.img(
+    image,
+    "./public/images",
+    new Date().getTime(),
+    async (err, filepath) => {
+      if (err) {
+        return res.status(400).json({ status: "error", message: err.message });
+      }
+
+      let imageName = filepath.split("/").pop();
+      const mediaRes = await Media.create({ imageUrl: `images/${imageName}` });
+
+      res.status(201).json({
+        status: "success",
+        data: {
+          id: mediaRes.id,
+          imageUrl: `${req.get("host")}/images/${imageName}`,
+        },
+      });
+    }
+  );
 });
 
 module.exports = router;
